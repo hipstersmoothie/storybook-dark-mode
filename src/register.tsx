8cdab3144718caa7223af8e4287e5e2542fae590
Andrew Lisowski <lisowski54@gmail.com>
2021-03-08 12:27:12 -0800

    when setting initial theme extends from default storybook themes to account for new variables that aren't yet stored it localStorage

--- src/register.tsx
+++ src/register.tsx
@@ -1,15 +1,18 @@
 import addons, { types } from '@storybook/addons';
+import { themes } from '@storybook/theming';
 import * as React from 'react';
 
 import Tool, { prefersDark, store } from './Tool';
 
 const currentStore = store();
+const currentTheme =
+  currentStore.current || (prefersDark.matches && 'dark') || 'light';
 
 addons.setConfig({
-  theme:
-    currentStore[
-      currentStore.current || (prefersDark.matches && 'dark') || 'light'
-    ]
+  theme: {
+    ...themes[currentTheme],
+    ...currentStore[currentTheme]
+  }
 });
 
 addons.register('storybook/dark-mode', api => {
