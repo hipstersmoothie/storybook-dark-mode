117c49684ac799888de1a4810d92c40182aa7e00
Slava Kostenko <kostenko.slav@gmail.com>
2023-02-10 21:18:26 +0400

    Fix importing global variables

--- src/Tool.tsx
+++ src/Tool.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import global from 'global';
+import { global } from '@storybook/global';
 import { themes, ThemeVars } from '@storybook/theming';
 import { IconButton } from '@storybook/components';
 import {
