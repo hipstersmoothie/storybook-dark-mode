153f2187bd872eecef55c09b204fa32fa2405f9d
Adam Gołąb <edek150@gmail.com>
2024-04-25 09:38:01 +0200

    fix: use useState hook from storybook preview api

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { addons } from '@storybook/preview-api';
+import { addons, useState } from '@storybook/preview-api';
 import { DARK_MODE_EVENT_NAME } from './constants';
 import { store } from './Tool';
 
@@ -7,7 +6,7 @@ import { store } from './Tool';
  * Returns the current state of storybook's dark-mode
  */
 export function useDarkMode(): boolean {
-  const [isDark, setIsDark] = React.useState(store().current === 'dark');
+  const [isDark, setIsDark] = useState(store().current === 'dark');
 
   React.useEffect(() => {
     const chan = addons.getChannel();
