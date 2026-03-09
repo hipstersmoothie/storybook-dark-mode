d10d8bfbed88929553645382f78290dcd7e403eb
Andrew Lisowski <lisowski54@gmail.com>
2026-03-09 11:59:38 -0700

    Upgrade to strorybook 10

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
-import { addons, useState, useEffect } from '@storybook/preview-api';
+import * as React from 'react';
+import { addons } from 'storybook/preview-api';
 import { DARK_MODE_EVENT_NAME } from './constants';
 import { store } from './Tool';
 
@@ -6,9 +7,9 @@ import { store } from './Tool';
  * Returns the current state of storybook's dark-mode
  */
 export function useDarkMode(): boolean {
-  const [isDark, setIsDark] = useState(store().current === 'dark');
+  const [isDark, setIsDark] = React.useState(store().current === 'dark');
 
-  useEffect(() => {
+  React.useEffect(() => {
     const chan = addons.getChannel();
     chan.on(DARK_MODE_EVENT_NAME, setIsDark);
     return () => chan.off(DARK_MODE_EVENT_NAME, setIsDark);
